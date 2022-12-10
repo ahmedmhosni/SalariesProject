a883beaa1bea64410fc1310d500ede04670c7047
Ahmed Hosni <40355764+ahmedmhosni@users.noreply.github.com>
2022-12-10 16:04:46 +0200

    The SQL Query File

---
+++ SQL_Salaries_Project.sql
@@ -0,0 +1,45 @@
+-- Select option
+-- To select exact top rows from the table, we have to write the number of row along with * astric
+SELECT TOP (1000) *
+FROM [Salaries]
+
+-- Time to know the available job titles
+SELECT DISTINCT(JobTitle)
+FROM [Salaries]
+
+-- Know the count of a column itmes
+SELECT COUNT (JobTitle)
+FROM [Salaries]
+
+-- To know number of unique items in the same column
+SELECT COUNT (DISTINCT(JobTitle))
+FROM [Salaries]
+
+-- check the max and min then check the avg
+SELECT MAX(TotalPay) , MIN(TotalPay), AVG(TotalPay)
+FROM [Salaries]
+
+
+-- Select some row/ columns (Where a spesfic data available is spesfic column)
+SELECT *
+FROM [Salaries]
+WHERE (JobTitle) = 'CAPTAIN III (POLICE DEPARTMENT)'
+
+-- Select some row/ columns (Where a spesfic data available is spesfic column) and condintions
+SELECT *
+FROM [Salaries]
+WHERE (JobTitle) = 'CAPTAIN III (POLICE DEPARTMENT)'
+AND (BasePay) < 100000
+
+-- Select rows started with spesfic letter
+SELECT *
+FROM [Salaries]
+WHERE (EmployeeName) LIKE 'P%'
+
+-- Conclusion
+-- Select rows started with spesfic letter LIKE 'P%'
+-- Select rows started with spesfic letter and include another spesfic letter inside LIKE 'P%O%'
+-- Select rows include  spesfic letter LIKE '%P%'
+SELECT *
+FROM [Salaries]
+WHERE (EmployeeName) LIKE 'P%'
