b99a3a90f089505eb7d6ef9dce06d6513c7c714c
ahmedmhosni <ahmedmhosni90@Gmail.com>
2023-03-21 18:56:40 +0200

    Analysis - police department data
    Some steps analyzing Police departments salaries

--- SQL_Salaries_Project.sql
+++ SQL_Salaries_Project.sql
@@ -2,23 +2,30 @@
 -- To select exact top rows from the table, we have to write the number of row along with * astric
 SELECT *
 FROM [Salaries]
-LIMIT ('1000')
+LIMIT ('20')
 
 -- Time to know the available job titles
 SELECT DISTINCT(JobTitle)
 FROM [Salaries]
 
+
+
 -- Know the count of a column itmes
 SELECT COUNT (JobTitle)
 FROM [Salaries]
+
+
 
 -- To know number of unique items in the same column
 SELECT COUNT (DISTINCT(JobTitle))
 FROM [Salaries]
 
+
+
 -- check the max and min then check the avg
 SELECT MAX(TotalPay) , MIN(TotalPay), AVG(TotalPay)
 FROM [Salaries]
+
 
 
 -- Select some row/ columns (Where a spesfic data available is spesfic column)
@@ -26,16 +33,22 @@ SELECT *
 FROM [Salaries]
 WHERE (JobTitle) = 'CAPTAIN III (POLICE DEPARTMENT)'
 
+
+
 -- Select some row/ columns (Where a spesfic data available is spesfic column) and condintions
 SELECT *
 FROM [Salaries]
 WHERE (JobTitle) = 'CAPTAIN III (POLICE DEPARTMENT)'
 AND (BasePay) < 100000
 
+
+
 -- Select rows started with spesfic letter
 SELECT *
 FROM [Salaries]
 WHERE (EmployeeName) LIKE 'P%'
+
+
 
 -- Conclusion
 -- Select rows started with spesfic letter LIKE 'P%'
@@ -46,7 +59,17 @@ FROM [Salaries]
 WHERE (EmployeeName) LIKE 'P%'
 
 
+
+
 -- Select all having spesfic keywords or words as follow
 SELECT *
 FROM [Salaries]
 WHERE (EmployeeName) LIKE ('PIERRE%')
+
+
+
+
+-- Count of each value from distinct (Complex)
+SELECT YEAR, Count (Year)
+FROM [Salaries]
+Group By Year
